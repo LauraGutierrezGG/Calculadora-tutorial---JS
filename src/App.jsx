5ac079102429539f7c1ea5cf2621f282d1512711
LauraGutierrezGG <lauragutierrezgallud@gmail.com>
2024-01-23 20:39:21 +0100

    numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
+import { useState } from 'react';
 import './App.scss'
 
 const actions = ['C', '+-', '%', '/', 7, 8, 9, 'X', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
 
 function App() {
+
+  const [value, setValue] = useState('0');
+
+  const handleClick = (newValue) => {
+    
+    if(value === '0'){
+      setValue(newValue);
+    } else {
+      setValue(value + newValue);
+    }
+
+    setValue(value + newValue);
+  };
+
   return (
     <div className="calculator">
-      <div className="calculator__result">0</div>
+
+      <div className="calculator__result">(value)
+      </div>
+
       <div className="calculator__actions">
+
         {actions.map((action) => (
-          <button key={action} className="calculator__action">
+
+          <button key={action} onClick={() => handleClick(action)} className="calculator__action">
             {action}
           </button>
+
         ))}
+
       </div>
+
     </div>
   );
 }
