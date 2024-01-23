365ceda225c7f73783b257e6f0dfda469a10eae0
LauraGutierrezGG <lauragutierrezgallud@gmail.com>
2024-01-23 19:05:20 +0100

    button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,56 @@
-import './App.css'
+import './App.scss'
 
 function App() {
 
-  return  <>hola</>;
+  return  <>
+
+    <div>
+
+      <div class='screen'>
+
+      </div>
+
+      <div class='teclado'>
+
+        <div class='firstline'>
+          <button>C</button>
+          <button>+-</button>
+          <button>%</button>
+          <button>/</button>
+        </div>
+
+        <div class='secondline'>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>X</button>
+        </div>
+
+        <div class='thirdline'>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+        </div>
+
+        <div class='fourthline'>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+        </div>
+
+        <div class='fifthline'>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+
+      </div>
+
+    </div>
+
+  </>;
   
 }
 
