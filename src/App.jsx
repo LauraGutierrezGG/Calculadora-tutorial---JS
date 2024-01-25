7cb4df7707865ecc29b320f117c4c843431325b2
LauraGutierrezGG <lauragutierrezgallud@gmail.com>
2024-01-25 18:59:20 +0100

    botton C added

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,47 @@
 import { useState } from 'react';
 import './App.scss'
 
-const actions = ['C', '+-', '%', '/', 7, 8, 9, 'X', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
-
 function App() {
 
   const [value, setValue] = useState('0');
 
-  const handleClick = (newValue) => {
+  const actions = ['C', '+-', '%', '/', 7, 8, 9, 'X', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
 
-    if (newValue == '.' && !value.includes('.')) {
-      setValue(value + newValue);
+  const handleClick = (actionClicked) => {
+    // Lógica para el botón "C"
+    if (actionClicked === 'C') {
+      setValue('0');
       return;
     }
 
-    if (typeof newValue !== 'number') {
+    if (actionClicked === '.' && !value.includes('.')) {
+      setValue(value + actionClicked);
       return;
     }
 
-    if(value === '0'){
-      setValue(newValue.toString());
+    if (typeof actionClicked !== 'number') {
+      return;
+    }
+
+    if (value === '0') {
+      setValue(actionClicked.toString());
     } else {
-      setValue(value + newValue);
+      setValue(value + actionClicked);
     }
-
-    setValue(value + newValue);
   };
 
   return (
     <div className="calculator">
-
-      <div className="calculator__result">(value)
-      </div>
-
+      <div className="calculator__result">{value}</div>
       <div className="calculator__actions">
-
         {actions.map((action) => (
-
           <button key={action} onClick={() => handleClick(action)} className="calculator__action">
             {action}
           </button>
-
         ))}
-
       </div>
-
     </div>
   );
 }
 
-export default App
+export default App;
